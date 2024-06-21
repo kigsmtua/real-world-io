e9097d946d1c7e50df53576cd59404867c7eb502
John Kiragu <mutuakiragu@gmail.com>
2024-06-21 16:49:15 +0300

    Allow for creation and registration of users on the user service

--- src/main/java/com/realworld/io/entities/User.java
+++ src/main/java/com/realworld/io/entities/User.java
@@ -1,7 +1,8 @@
 package com.realworld.io.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -9,16 +10,17 @@ import java.util.Date;
 
 @Table(name = "users")
 @Entity
-@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long integer;
 
     @Column(unique = true, length = 100, nullable = false)
+    @Getter @Setter
     private String email;
 
     @Column(nullable = false)
+    @Getter @Setter
     private String password;
 
     @CreationTimestamp
